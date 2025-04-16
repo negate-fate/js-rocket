9a859ce0754b3cf4c903cb62d26015a4ea3081fa
negate-fate <mark.wallman@i-nex.com.au>
2025-04-16 14:05:38 +0930

    Create explanations.js

---
+++ explanations.js
@@ -0,0 +1,33 @@
+// Explanations
+
+// variables (lesson 7)
+
+var favColor = 'blue';
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = false;
+var richObject = {
+	firstName: "Rich",
+	lastName: "Armstrong",
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 103,
+	isMale: true
+};
+
+// functions (lesson 8)
+
+var whatIsMyFavColor = function () {
+	return 'orange';
+}
+
+var doSomething = function () {
+	console.log("do something!!!");
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var fullname = function (firstname, secondname) {
+	return firstname + " " + secondname;
+}
